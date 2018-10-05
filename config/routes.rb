eda2b00a576ce34d49b04cb33e3ced356a805e03
tty007 <c7dc007@gmail.com>
2018-10-05 22:29:04 +0900

    投稿ボタンの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 require 'sidekiq/web'
 Rails.application.routes.draw do
+  get 'posts/create'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
   mount Sidekiq::Web => '/sidekiq'
+  resources :posts, only: [:create]
 end
